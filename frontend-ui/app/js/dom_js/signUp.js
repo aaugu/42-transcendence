b8d74351ba32b34b9c0a834193ea1fdae004184a
Christina Karl <ckarl@c3r3s1.42lausanne.ch>
2024-07-08 18:22:23 +0200

    todo for tmrw

--- frontend-ui/app/js/dom_js/signUp.js
+++ frontend-ui/app/js/dom_js/signUp.js
@@ -4,4 +4,8 @@ export function signUpButton() {
     const userName = inputValue('username');
     const nickName = inputValue('nickname');
     const email = inputValue('email');
+
+    //add functionality to delete when sign up is clicked
+    //add redirection when signup is clicked
+    //add variables to localstorage + create variable userLoggedIn == true
 }
