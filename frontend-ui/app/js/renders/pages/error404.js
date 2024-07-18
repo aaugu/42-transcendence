779af0d042ef94d0117724271b0735c5ba7df4ef
Anais Augu <aaugu@c3r2s4.42lausanne.ch>
2024-07-18 11:43:57 +0200

    new game page ok

--- frontend-ui/app/js/renders/pages/error404.js
+++
@@ -1,5 +0,0 @@
-export function error404() {
-    return `
-    <h1 class="text-bold display-5">Page not found</h1>
-    `;
-}
--- frontend-ui/app/js/renders/pages/home.js
+++
@@ -1,6 +0,0 @@
-export function homePage() {
-    return `
-    <h1 class="text-bold text-center display-5">Welcome</h1>
-    `;
-}
-
