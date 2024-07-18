3bc29d20fc5069e4ddf50f6badaf81631182cba5
Anais Augu <aaugu@c3r2s4.42lausanne.ch>
2024-07-18 12:47:21 +0200

    tournament home render ok

--- frontend-ui/app/js/renders/pages/newGame.js
+++ frontend-ui/app/js/renders/pages/newGame.js
@@ -1,11 +1,15 @@
 let html = `
-    <div class="h-50 titles d-flex flex-column justify-content-around align-items-center py-3">
-        <h1 class="main-content-title text-center">New Game</h1>
-        <h3 class="main-content-sub-title w-75 text-center">Choose your type of game</h3>
+    <div class="h-50 titles d-flex flex-column justify-content-around align-items-center">
+        <div class="h1 main-content-title text-center">New Game</div>
+        <div class="h3 main-content-sub-title w-75 text-center">Choose your type of game</div>
     </div>
-    <div class="mw-50 h-50 buttons d-flex justify-content-around align-items-center">
-        <div class="btn btn-lightgrey text-dark text-break py-4 mb-2 w-100">2 Player Game</div>
-        <div class="btn btn-lightgrey text-dark text-break py-4 mb-2 w-100"><a href="/tournament-home">Tournament Game</a></div>
+    <div class="buttons h-50 d-flex flex-column flex-md-row justify-content-around align-items-center">
+        <div class="select-btn btn btn-lightgrey w-100 d-flex justify-content-center align-items-center" id="2p-btn">
+            <div>2 Player Game</div>
+        </div>
+        <div class="select-btn btn btn-lightgrey w-100 d-flex justify-content-center align-items-center" id="tournament-home-btn">
+            <a href="#" class="text-break">Tournament Game</a>
+        </div>
     </div>
 `;
 
--- frontend-ui/app/js/renders/pages/tournamentHome.js
+++ frontend-ui/app/js/renders/pages/tournamentHome.js
@@ -1,11 +1,17 @@
 let html = `
-    <div class="h-50 titles d-flex flex-column justify-content-around align-items-center py-3">
-        <h1 class="main-content-title text-center">Tournament</h1>
-        <h3 class="main-content-sub-title w-75 text-center">Do you want to join an existing tournament or create your own ?</h3>
-    </div>
-    <div class="mw-50 h-50 buttons d-flex justify-content-around align-items-center">
-        <div class="btn btn-dark py-3 mb-2 w-100"><a class="text-white text-break" href="/tournaments">Join tournament</a></div>
-        <div class="btn btn-lightgrey py-3 mb-2 w-100"><a class="text-break" href="/new-tournament">Create Tournament</a></div>
+    <div id="main-content" class="h-75 bg-white p-4">
+        <div class="h-50 titles d-flex flex-column justify-content-around align-items-center">
+            <div class="text-center h1">Tournament</div>
+            <p class="text-center w-75">Do you want to join an existing tournament or create your own ?</p>
+        </div>
+        <div class="buttons h-50 d-flex flex-column flex-md-row justify-content-around align-items-center">
+            <div class="select-btn btn btn-dark w-100 d-flex justify-content-center align-items-center" id="join-tournament-btn">
+                <a class="text-white text-break" href="/tournaments">Join tournament</a>
+            </div>
+            <div class="select-btn btn btn-lightgrey w-100 d-flex justify-content-center align-items-center" id="create-tournament-btn">
+                <a class="text-break" href="/new-tournament">Create Tournament</a>
+            </div>
+        </div>
     </div>
 `;
 
