a635ec2631c65b40b2f1fa6c1486f7168a21b6c2
Anais Augu <aaugu@c3r2s3.42lausanne.ch>
2024-07-23 10:51:47 +0200

    micro service base

--- common/generate_postgres_config.sh
+++ common/generate_postgres_config.sh
@@ -8,3 +8,9 @@ echo $AETHERYTE_SQL > aetheryte_api_gateway/docker/postgres/config/init.sql
 # Micro service TOURNAMENT
 
 # Micro service USER MANAGEMENT
+
+# Micro service LIVE CHAT
+
+LIVECHAT_POSTGRES_USER=$(cat .env | grep LIVECHAT_POSTGRES_USER= | cut -d '=' -f2)
+LIVECHAT_SQL="ALTER USER ${LIVECHAT_POSTGRES_USER} WITH superuser createrole createdb replication bypassrls;"
+echo $LIVECHAT_SQL > livechat/docker/postgres/config/init.sql
