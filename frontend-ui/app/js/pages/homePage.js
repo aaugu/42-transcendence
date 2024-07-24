e15eeb4468b8313507c7553714ca2dab3bdc001a
christinakarl <christina.karl@posteo.de>
2024-07-24 16:48:03 +0200

    profile page flexbox layout ok

--- frontend-ui/app/js/pages/homePage.js
+++ frontend-ui/app/js/pages/homePage.js
@@ -2,11 +2,23 @@ import { isLoggedIn } from "./login/isLoggedIn.js";
 
 export function homePage() {
     return `
-    <h1 class="text-bold display-5">Come play pong</h1>
-    <br>
-    <row class="justify-content-center mt-5">
-		<a type="submit" class="btn btn-light" id="login" href="/login">Log in</a>
-		<a type="submit" class="btn btn-dark" id="signup" href="/signup">Sign up</a>
-	</row>
+    <h2 class="text-bold display-5">Profile</h1>
+    <div class="column">
+        <div class="profile-row">
+            <div class="profile-column-left">
+                <div id="user-management" class="profile-box  clearfix">user management</div>
+                <div id="2fa" class="profile-box clearfix">2fa authentication
+                    <button type="text" class="btn btn-success" id="2fa-button">Activate</button>
+                </div>
+            </div>
+            <div class="profile-column-right">
+                <div id="personal-stats" class="profile-box clearfix" style="height: 200px;">stats</div>
+                <div id="friends" class="profile-box">friends</div>
+            </div>
+        </div>
+        <row class="justify-content-center">
+            <button type="text" class="btn btn-danger" id="logout">Logout</button>
+        </row>
+    </div>
     `;
 }
--- frontend-ui/app/js/pages/profile/profilePage.js
+++ frontend-ui/app/js/pages/profile/profilePage.js
@@ -1,26 +1,11 @@
 export function profilePage() {
 
     return `
-    <h1 class="text-bold display-3">Profile!</h1>
+    <h1 class="text-bold display-5">Come play pong</h1>
     <br>
-    <h2 class="small">Some information about yourself</h2>
-	<div class="flex-container">
-	<div class="flex-col">
-		<div class="large-box">
-		</div>
-	</div>
-	<div class="flex-col">
-		<div class="small-box">
-		</div>
-
-	</div>
-		<div class="medium-box">
-	</div>
-		<div class="medium-box">
-	</div>
-	</div>
-	<row class="justify-content-center">
-        <button type="text" class="btn btn-danger" id="logout">Logout</button>
-    </row>
+    <row class="justify-content-center mt-5">
+		<a type="submit" class="btn btn-light" id="login" href="/login">Log in</a>
+		<a type="submit" class="btn btn-dark" id="signup" href="/signup">Sign up</a>
+	</row>
     `;
 }
