70569995c833735b51a36d07573a3edb18f87c5d
christinakarl <christina.karl@posteo.de>
2024-07-24 22:43:30 +0200

    profile page white boxes ready

--- frontend-ui/app/js/pages/homePage.js
+++ frontend-ui/app/js/pages/homePage.js
@@ -1,19 +1,28 @@
 import { isLoggedIn } from "./login/isLoggedIn.js";
 
 export function homePage() {
+    const mainCont = document.getElementById("main-content");
+    mainCont.style.backgroundColor = "rgba(255, 255, 255, 0)";
     return `
-    <h2 class="text-bold display-5">Profile</h1>
+    <h2 class="text-bold display-5">Profile</h2>
     <div class="column">
         <div class="profile-row">
             <div class="profile-column-left">
-                <div id="user-management" class="profile-box  clearfix">user management</div>
-                <div id="2fa" class="profile-box clearfix">2fa authentication
-                    <button type="text" class="btn btn-success" id="2fa-button">Activate</button>
+                <div id="user-management" class="profile-box clearfix">
+                    <div class="text-sm m-2">User management</div>
+                </div>
+                <div id="2fa" class="profile-box clearfix">
+                    <div class="text-sm m-2">2fa authentication</div>
+                    <button type="text" class="btn btn-success align-items-center m-2" id="2fa-button">Activate</button>
                 </div>
             </div>
             <div class="profile-column-right">
-                <div id="personal-stats" class="profile-box clearfix" style="height: 200px;">stats</div>
-                <div id="friends" class="profile-box">friends</div>
+                <div id="personal-stats" class="profile-box clearfix" style="height: 200px;">
+                    <div class="text-sm m-2">Personal stats</div>
+                </div>
+                <div id="friends" class="profile-box">
+                    <div class="text-sm m-2">Friend list</div>
+                </div>
             </div>
         </div>
         <row class="justify-content-center">
