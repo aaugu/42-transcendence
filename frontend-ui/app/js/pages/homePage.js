5892d365ae07ab40b78ff2f8a2836c6a32ff7db6
christinakarl <christina.karl@posteo.de>
2024-07-26 16:37:39 +0200

    game history list, not yet scrollable

--- frontend-ui/app/js/pages/homePage.js
+++ frontend-ui/app/js/pages/homePage.js
@@ -16,19 +16,37 @@ export function homePage() {
             </div>
         </div>
         <div class="profile-column-right">
-            <div id="personal-stats" class="profile-box cleafix" style="height: 200px;">
+            <div id="personal-stats" class="profile-box clearfix">
                 <h5 class="m-2">Personal stats</h5>
-                <div class="profile-box justify-content-center " style="flex-direction: row;">
+                <div class="profile-box" style="flex-direction: row; justify-content: space-evenly;">
                     <div>
-                        <img src="images/trophy.jpg" alt="" class="thumb-icon">
-                        <div class="text-sm m-2">xyz</div>
+                        <img src="images/trophy.png" alt="" class="profile-icon">
+                        <div class="text-sm m-2">4</div>
                     </div>
                     <div>
-                        <img src="images/thumb_down_red.jpg" alt="" class="thumb-icon">
-                        <div class="text-sm m-2">xyz</div>
+                        <img src="images/thumb_down.webp" alt="" class="profile-icon">
+                        <div class="text-sm m-2">3</div>
                     </div>
                 </div>
-                <div class="text-sm m-2">Game history</div>
+                <div class="profile-box" style="flex-direction: row;">
+                    <ul class="list-group m-2">
+                        <li class="list-group-item ">
+                            <span class="col col-sm-2">25/07/2024</span>
+                            <span class="col col-sm-4 text-truncate">Opponent2</span>
+                            <span class="col col-sm-2">WON</span>
+                        </li>
+                        <li class="list-group-item">
+                            <span class="col col-sm-2">25/07/2024</span>
+                            <span class="col col-sm-4 text-truncate">OpponentOpponent2OpponentOpponent2OpponentOpponent2OpponentOpponent2OpponentOpponent2OpponentOpponent2Opponent2Opponent</span>
+                            <span class="col col-sm-2"">LOST</span>
+                        </li>
+                        <li class="list-group-item">
+                            <span class="col col-sm-2">25/07/2024</span>
+                            <span class="col col-sm-4 text-truncate">blabla</span>
+                            <span class="col col-sm-2">LOST</span>
+                        </li>
+                    </ul>
+                </div>
             </div>
             <div id="friends" class="profile-box">
                 <h5 class="m-2">Friend list</h5>
