d2dad45e332086d1909aa3bccb6406e911c999c5
christinakarl <christina.karl@posteo.de>
2024-07-29 16:19:26 +0200

    profile page user management done, stats list not yet responsive

--- frontend-ui/app/js/pages/homePage.js
+++ frontend-ui/app/js/pages/homePage.js
@@ -1,60 +1,14 @@
 import { isLoggedIn } from "./login/isLoggedIn.js";
 
 export function homePage() {
-    const mainCont = document.getElementById("main-content");
-    mainCont.style.backgroundColor = "rgba(245, 245, 245, 0)";
     return `
-    <h2 class="text-bold display-6">Profile</h2>
-    <div class="profile-row">
-        <div class="profile-column-left">
-            <div id="user-management" class="profile-box clearfix">
-                <h5 class="m-2">User management</h5>
-            </div>
-            <div id="2fa" class="profile-box clearfix">
-                <h5 class="m-2">2fa authentication</h5>
-                <button type="text" class="selected btn btn-success m-2" id="2fa-button">Activate</button>
-            </div>
-        </div>
-        <div class="profile-column-right">
-            <div id="personal-stats" class="profile-box clearfix">
-                <h5 class="m-2">Personal stats</h5>
-                <div class="profile-box" style="flex-direction: row; justify-content: space-evenly;">
-                    <div>
-                        <img src="images/trophy.png" alt="" class="profile-icon">
-                        <div class="text-sm m-2">4</div>
-                    </div>
-                    <div>
-                        <img src="images/thumb_down.webp" alt="" class="profile-icon">
-                        <div class="text-sm m-2">3</div>
-                    </div>
-                </div>
-                <div class="profile-box" style="flex-direction: row;">
-                    <ul class="list-group m-2">
-                        <li class="list-group-item ">
-                            <span class="col col-sm-2">25/07/2024</span>
-                            <span class="col col-sm-4 text-truncate">Opponent2</span>
-                            <span class="col col-sm-2">WON</span>
-                        </li>
-                        <li class="list-group-item">
-                            <span class="col col-sm-2">25/07/2024</span>
-                            <span class="col col-sm-4 text-truncate">OpponentOpponent2OpponentOpponent2OpponentOpponent2OpponentOpponent2OpponentOpponent2OpponentOpponent2Opponent2Opponent</span>
-                            <span class="col col-sm-2"">LOST</span>
-                        </li>
-                        <li class="list-group-item">
-                            <span class="col col-sm-2">25/07/2024</span>
-                            <span class="col col-sm-4 text-truncate">blabla</span>
-                            <span class="col col-sm-2">LOST</span>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-            <div id="friends" class="profile-box">
-                <h5 class="m-2">Friend list</h5>
-            </div>
-        </div>
+    <div class="profile-box">
+        <h1 class="text-bold display-5">Come play pong</h1>
+        <br>
+        <row class="justify-content-center mt-5">
+            <a type="submit" class="btn btn-light" id="login" href="/login">Log in</a>
+            <a type="submit" class="btn btn-dark" id="signup" href="/signup">Sign up</a>
+        </row>
     </div>
-    <row class="justify-content-center">
-        <button type="text" class="btn btn-danger" id="logout">Logout</button>
-    </row>
     `;
 }
--- frontend-ui/app/js/pages/profile/profilePage.js
+++ frontend-ui/app/js/pages/profile/profilePage.js
@@ -1,11 +1,84 @@
 export function profilePage() {
-
+    const mainCont = document.getElementById("main-content");
+    mainCont.style.backgroundColor = "rgba(245, 245, 245, 0)";
     return `
-    <h1 class="text-bold display-5">Come play pong</h1>
-    <br>
-    <row class="justify-content-center mt-5">
-		<a type="submit" class="btn btn-light" id="login" href="/login">Log in</a>
-		<a type="submit" class="btn btn-dark" id="signup" href="/signup">Sign up</a>
-	</row>
+    <h2 class="text-bold display-6"></h2>
+    <div class="profile-row">
+        <div class="profile-column-left">
+            <div id="user-management" class="profile-box clearfix">
+                <h5 class="m-2">User management</h5>
+                <div class="profile-details">
+                    <div class="usermanagement-item centered">
+                        <img id="profile-avatar" src="images/default_avatar.png" alt="User Avatar" class="avatar-img">
+                        <button id="edit-avatar" class="edit-btn"><i class="fas fa-pen"></i></button>
+                    </div>
+                    <div class="usermanagement-item">
+                        <p id="profile-username">"Username"</p>
+                        <button id="edit-username" class="edit-btn"><i class="fas fa-pen"></i></button>
+                    </div>
+                    <div class="usermanagement-item">
+                        <p id="profile-nickname">"Nickname"</p>
+                        <button id="edit-nickname" class="edit-btn"><i class="fas fa-pen"></i></button>
+                    </div>
+                    <div class="usermanagement-item">
+                        <p id="profile-email">"E-mail@email.com"</p>
+                        <button id="edit-email" class="edit-btn"><i class="fas fa-pen"></i></button>
+                    </div>
+                    <div class="usermanagement-item">
+                        <p id="profile-password">Password</p>
+                        <button id="edit-password" class="edit-btn"><i class="fas fa-pen"></i></button>
+                    </div>
+                </div>
+            </div>
+            <div id="2fa" class="profile-box clearfix">
+                <h5 class="m-2">2fa authentication</h5>
+                <button type="text" class="centered btn btn-outline-success m-2" id="2fa-button">Activate</button>
+            </div>
+        </div>
+        <div class="profile-column-right">
+            <div id="personal-stats" class="profile-box clearfix">
+                <h5 class="m-2">Personal stats</h5>
+                <div class="profile-details">
+                    <p>Total wins: 3</p>
+                    <p>Total losses: 2</p>
+                </div>
+                <div>
+                <ul class="list-group overflow-auto m-2" style="max-height: 100px;">
+                    <li class="list-group-item">
+                        <text>Date</text>
+                        <text>Opponent</text>
+                        <text>WON/LOST</text>
+                    </li>
+                    <li class="list-group-item">
+                        <text>Date</text>
+                        <text>Opponent</text>
+                        <text>WON/LOST</text>
+                    </li>
+                    <li class="list-group-item">
+                        <text>Date</text>
+                        <text>Opponent</text>
+                        <text>WON/LOST</text>
+                    </li>
+                    <li class="list-group-item">
+                        <text>Date</text>
+                        <text>Opponent</text>
+                        <text>WON/LOST</text>
+                    </li>
+                    <li class="list-group-item">
+                        <text>Date</text>
+                        <text>Opponent</text>
+                        <text>WON/LOST</text>
+                    </li>
+                </ul>
+                </div>
+            </div>
+            <div id="friends" class="profile-box">
+                <h5 class="m-2">Friend list</h5>
+            </div>
+        </div>
+    </div>
+    <row class="justify-content-center">
+        <button type="text" class="btn btn-danger" id="logout">Logout</button>
+    </row>
     `;
 }
