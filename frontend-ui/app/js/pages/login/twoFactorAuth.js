e2223a5eca9e8b986815510cf2ab045b0e3382b2
christinakarl <christina.karl@posteo.de>
2024-08-07 11:00:33 +0200

    upd

--- frontend-ui/app/js/pages/login/twoFactorAuth.js
+++ frontend-ui/app/js/pages/login/twoFactorAuth.js
@@ -1,19 +1,19 @@
 export var twoFactorAuth = (localStorage.getItem("twoFactorAuth") === "true");
 
 export function twoFactorAuthButton() {
-	const FAbtn = document.getElementById("twoFactorAuth-btn");
+	const twoFAbtn = document.getElementById("twoFactorAuth-btn");
 	console.log("two factor value in local storage: ", twoFactorAuth);
 	if (twoFactorAuth === false) {
 		console.log("twoFactorAuth is true");
-		FAbtn.innerHTML = "Deactivate";
-		FAbtn.classList.remove("btn-outline-success");
-		FAbtn.classList.add("btn-outline-danger");
+		twoFAbtn.innerHTML = "Deactivate";
+		twoFAbtn.classList.remove("btn-outline-success");
+		twoFAbtn.classList.add("btn-outline-danger");
 		localStorage.setItem('twoFactorAuth', true);
 		twoFactorAuth = true;
 	} else {
-		FAbtn.innerHTML = "Activate";
-		FAbtn.classList.remove("btn-outline-danger");
-		FAbtn.classList.add("btn-outline-success");
+		twoFAbtn.innerHTML = "Activate";
+		twoFAbtn.classList.remove("btn-outline-danger");
+		twoFAbtn.classList.add("btn-outline-success");
 		localStorage.setItem('twoFactorAuth', false);
 		twoFactorAuth = false;
 	}
