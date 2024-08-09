e07e48838aa1d7b0a4f7065a2e1188b4c0a0cadf
Anais Augu <aaugu@c3r1s4.42lausanne.ch>
2024-08-09 14:58:53 +0200

    simulation to see each user conversations

--- livechat/app/liveChatProject/livechat/models.py
+++ livechat/app/liveChatProject/livechat/models.py
@@ -2,15 +2,24 @@ from django.db import models
 from django.utils import timezone, dateformat
 
 # Create your models here.
+class User(models.Model):
+    username = models.CharField()
+
+    def __str__(self):
+        return self.username
+
 class Conversation(models.Model):
-    user1 = models.PositiveIntegerField()
-    user2 = models.PositiveIntegerField()
+    user_1 = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user_1")
+    user_2 = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user_2")
 
 class Message(models.Model):
     conversation = models.ForeignKey(Conversation, on_delete=models.CASCADE)
     sender = models.PositiveIntegerField(blank=False)
     message = models.TextField(blank=False)
     timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.sender
 
     def date(self):
         return dateformat.format(timezone.localtime(self.timestamp), 'Y-m-d')
