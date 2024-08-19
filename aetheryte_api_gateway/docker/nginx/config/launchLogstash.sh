4efd982c2cec1fcbc4590f44a99b575a185ec72a
Arnaud FAVRE <afavre@student.42lausanne.ch>
2024-08-19 14:43:07 +0200

    pourquoi pas

--- aetheryte_api_gateway/docker/nginx/config/launchLogstash.sh
+++ aetheryte_api_gateway/docker/nginx/config/launchLogstash.sh
@@ -3,3 +3,4 @@
 echo "-------------------- Demarage de logstash --------------------"
 systemctl start logstash
 echo "-------------------- Demarage terminee --------------------"
+echo "PS : je n'aime pas les capibara !"
