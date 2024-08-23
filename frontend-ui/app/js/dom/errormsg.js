9deca83ecd6330ad52d0d5730193f2d021553552
favrarnaud <afavre@student.42lausanne.ch>
2024-08-23 14:09:37 +0200

    remove front file

--- frontend-ui/app/js/dom/errormsg.js
+++
@@ -1,9 +0,0 @@
-export async function errormsg(msg, id) {
-    const errorCont = document.getElementById(id);
-
-    errorCont.classList.remove('hidden');
-    errorCont.textContent = msg;
-    setTimeout(() => {
-        errorCont.classList.add('hidden');
-    }, 3000);
-}
