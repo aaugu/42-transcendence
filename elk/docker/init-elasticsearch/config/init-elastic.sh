626bbe16af90ec3e08f4bf9c589a565a967b214d
favrarnaud <afavre@student.42lausanne.ch>
2024-09-02 11:11:28 +0200

    rework init elk scripts

--- elk/docker/init-elasticsearch/config/init-elastic.sh
+++ elk/docker/init-elasticsearch/config/init-elastic.sh
@@ -1,10 +1,15 @@
-#!/bin/bash
+CYAN='\033[0;36m'
+ROUGE='\033[0;31m'
+VERT='\033[0;32m'
+NC='\033[0m'
 
 # Vérifiez que les variables d'environnement nécessaires sont définies
 if [ -z "$ELASTIC_PASSWORD" ] || [ -z "$ELASTICSEARCH_USERNAME" ]; then
-  echo "Les variables ELASTIC_PASSWORD et ELASTICSEARCH_USERNAME doivent être définies dans le fichier .env."
+  echo "$ROUGE---> Les variables ELASTIC_PASSWORD et ELASTICSEARCH_USERNAME doivent être définies dans le fichier .env.$NC"
   exit 1
 fi
+
+echo "$CYAN-------------------- GENERATING ELASTIC USER GROUP... --------------------$NC"
 
 # Attendre que Elasticsearch soit disponible avec authentification
 until curl -s -u elastic:"$ELASTIC_PASSWORD" http://172.20.6.2:9200/_cluster/health?pretty > /dev/null; do
@@ -33,18 +38,20 @@ create_or_update_role_response=$(curl -s -o /dev/null -w "%{http_code}" -X PUT "
 
 # Vérifier le code de retour de la création du rôle
 if [ "$create_or_update_role_response" -ne 200 ] && [ "$create_or_update_role_response" -ne 201 ]; then
-  echo "Failed to create role custom_kibana_admin. HTTP status code: $create_or_update_role_response"
+  echo "$ROUGEFailed to create role custom_kibana_admin. HTTP status code: $create_or_update_role_response $NC"
   exit 1
 else
-  echo "Role custom_kibana_admin created successfully."
+  echo "$VERT Role custom_kibana_admin created successfully.$NC"
 fi
+
+echo "$CYAN-------------------- GENERATING ELASTIC USER... --------------------$NC"
 
 # Créer un utilisateur avec le rôle custom_kibana_admin
 create_user_response=$(curl -s -o /dev/null -w "%{http_code}" -X POST "http://172.20.6.2:9200/_security/user/$ELASTICSEARCH_USERNAME" -H "Content-Type: application/json" -u elastic:"$ELASTIC_PASSWORD" -d "{\"password\" : \"$ELASTIC_PASSWORD\", \"roles\" : [\"custom_kibana_admin\"]}")
 
 if [ "$create_user_response" -eq 200 ] || [ "$create_user_response" -eq 201 ]; then
-  echo "User $ELASTICSEARCH_USERNAME created with custom_kibana_admin role."
+  echo "$VERT User $ELASTICSEARCH_USERNAME created with custom_kibana_admin role.$NC"
 else
-  echo "Failed to create user $ELASTICSEARCH_USERNAME. HTTP status code: $create_user_response"
+  echo "$ROUGE Failed to create user $ELASTICSEARCH_USERNAME. HTTP status code: $create_user_response $NC"
   exit 1
 fi
--- elk/docker/init-elasticsearch/config/init-kibana.sh
+++ elk/docker/init-elasticsearch/config/init-kibana.sh
@@ -1,14 +1,16 @@
 CYAN='\033[0;36m'
+ROUGE='\033[0;31m'
+VERT='\033[0;32m'
 NC='\033[0m'
 
 if [ -f .env ]; then
   export $(grep -v '^#' .env | xargs)
 fi
 
-echo "$CYAN-------------------- GENERATING KIBANA MODEL VIEW... --------------------$NC"
+echo "\n$CYAN-------------------- GENERATING KIBANA MODEL VIEW... --------------------$NC"
 
 until curl -s -X GET "http://172.20.6.3:5601/api/status" | grep -q '"level":"available"'; do
-  echo "Attente du démarrage de Kibana..."
+  echo "Waiting for  Kibana..."
   sleep 5
 done
 
@@ -25,4 +27,4 @@ curl -X POST "http://172.20.6.3:5601/api/saved_objects/index-pattern/my-data-vie
       "timeFieldName": "@timestamp"
     }
   }'
-echo "\n$CYAN-------------------- KIBANA MODEL VIEW SUCCESSFULLY UPDATED --------------------$NC"
+echo "\n$VERT-------------------- KIBANA MODEL VIEW SUCCESSFULLY UPDATED --------------------$NC"
