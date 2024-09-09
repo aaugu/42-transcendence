d5d6cd4dadd6f65e95b50eec332454692319c845
natevaub <nathan.vaubien@gmail.com>
2024-09-09 23:13:52 +0200

    Removed Tracked Garbage Files

--- frontend-ui/app/js/pages/game/gameplay/WebSocketConnection.js
+++ frontend-ui/app/js/pages/game/gameplay/WebSocketConnection.js
@@ -1,21 +1,14 @@
+import {userID} from "../../user/updateProfile.js";
+
 export default function createWebSocketConnection() {
   // const socket = new WebSocket("ws://" + window.location.host + "/ws/pong/");
 
   // Generate a unique ID
+
   const currentUrl = window.location.href;
-  console.log("Current URL: ", currentUrl);
 
   const mode = currentUrl.split("/")[3];
-  console.log("Mode: ", mode);
 
-  const gameId = generateUniqueId(mode);
-  console.log("Game ID: ", gameId);
+  console.log(`Game and URLS: ${currentUrl} ${mode} ${userID}`);
 
-  const socket = new WebSocket(`ws://localhost:9000/ws/pong/${gameId}`);
-
-  socket.onopen = function () {
-    console.log("GAME LOG: Websocket connection established");
-  };
-
-  return socket;
 }
