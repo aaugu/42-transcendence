eed9ebd66348f35c1c31006839f3a454568e1c0d
natevaub <nathan.vaubien@gmail.com>
2024-09-10 16:04:44 +0200

    Improvement Game, Websocket Creation Game

--- frontend-ui/app/js/pages/game/gameplay/WebSocketConnection.js
+++ frontend-ui/app/js/pages/game/gameplay/WebSocketConnection.js
@@ -1,16 +1,23 @@
-import {userID} from "../../user/updateProfile.js";
+import { userID } from "../../user/updateProfile.js";
 
 const gatewayEndpoint = "https://localhost:10444/pong";
 
 export async function createGame(userID, mode) {
-  const response = await fetch(`${gatewayEndpoint}/create-game/${userID}/${mode}`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-  });
+  const response = await fetch(
+    `${gatewayEndpoint}/create-game/${userID}/${mode}/`,
+    {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      credentials: "include",
+    }
+  );
 
   const data = await response.json();
+
+  console.log(JSON.stringify(data));
+
   return data;
 }
 
@@ -21,12 +28,31 @@ export default async function createWebSocketConnection() {
 
   const currentUrl = window.location.href;
 
-  const mode = currentUrl.split("/")[3];
+  var mode = currentUrl.split("/")[3];
 
-  console.log(`Game and URLS: ${currentUrl} ${mode} ${userID}`);
+  console.log(`MODE: ${mode}, USER ID: ${userID}`);
+
+
+  switch (mode) {
+    case "local-twoplayer":
+      mode = "LOCAL_TWO_PLAYERS";
+      break;
+    // case "local-oneplayer":
+    //   mode = "LOCAL_ONE_PLAYER";
+    //   break;
+    // case "online-twoplayer":
+    //   mode = "ONLINE_TWO_PLAYERS";
+    //   break;
+    // case "online-oneplayer":
+    //   mode = "ONLINE_ONE_PLAYER";
+    //   break;
+    default:
+      console.log("Invalid mode");
+  }
 
   const gameData = await createGame(userID, mode);
 
-  console.log(gameData);
+  const socket = new WebSocket(`ws://localhost:9000/ws/pong/${gameData.game_id}/`);
 
+  return socket;
 }
