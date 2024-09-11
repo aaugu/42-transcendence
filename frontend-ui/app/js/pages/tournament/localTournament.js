dc6b732f5e4fdd6427a6bd66a02edce77c485c2f
Christina Karl <ckarl@c3r2s4.42lausanne.ch>
2024-09-11 18:31:28 +0200

    startMatches started

--- frontend-ui/app/js/pages/tournament/localTournament.js
+++ frontend-ui/app/js/pages/tournament/localTournament.js
@@ -1,9 +1,11 @@
 import { urlRoute } from '../../dom/router.js';
 import { startGame } from '../game/gameplay/startGame.js';
 import { generateMatches } from './match.js';
+import { startTournament } from './startTournament.js';
 
 export async function startLocalTournament() {
     const tourn_id = localStorage.getItem('tourn_id');
+    localStorage.removeItem('tourn_id');
     if (tourn_id === null || tourn_id === undefined) {
         console.log("USER LOG: Tournament ID not found");
         // urlRoute('/tournament');
@@ -12,9 +14,14 @@ export async function startLocalTournament() {
         const response = await generateMatches(tourn_id, 'POST');
         console.log("matches: ", response);
 
-        //startTournamentView (patch), give tourn_id
-        //startMatchView (post), give tourn_id, body: player1, player2
+        await startTournament(tourn_id);
+        //startMatchView (post), give tourn_id, body: player1, player2 from generateMatches
+        // await startMatch(tourn_id, response[0].player1, response[0].player2);
+
+        //do this in startGame??
         startGame();
+
+
     }
     catch (e) {
         console.error(`USER LOG: ${e.message}`);
--- frontend-ui/app/js/pages/tournament/startTournament.js
+++ frontend-ui/app/js/pages/tournament/startTournament.js
@@ -3,7 +3,7 @@ import { hideModal } from '../../dom/modal.js';
 import { userID } from '../user/updateProfile.js';
 import { generateMatches } from './match.js';
 
-async function startTournament(tournament_id) {
+export async function startTournament(tournament_id) {
 	const url = 'https://localhost:10444/api/tournament/' + tournament_id + '/start/';
 	const response = await fetch(url, {
 		method: 'PATCH',
@@ -33,11 +33,14 @@ export async function startTournamentButton() {
 	try {
 		const tourn_id = document.getElementById('t-start').dataset.tournid;
 
-		const matches = await generateMatches(tourn_id);
-		console.log('matches: ', matches);
-		await startTournament(tourn_id);
-		document.getElementById('t-start').classList.add('hidden');
-		hideModal('single-t-modal');
+		// const matches = await generateMatches(tourn_id);
+		// console.log('matches: ', matches);
+		// await startTournament(tourn_id);
+		// document.getElementById('t-start').classList.add('hidden');
+		// hideModal('single-t-modal');
+
+		localStorage.setItem('tourn_id', tourn_id);
+		// urlRoute('/tournament/remote');
 	}
 	catch (e) {
 		const tournament_name = document.getElementById('single-t-modal-title').innerText;
