5d5ae340ef180d5829830fbc759b806cf4aa49fc
christinakarl <christina.karl@posteo.de>
2024-10-04 23:20:18 +0200

    passwordValidity upd

--- frontend-ui/app/js/pages/user/password.js
+++ frontend-ui/app/js/pages/user/password.js
@@ -5,7 +5,7 @@ export function passwordValidity (password) {
     const hasUpperCase = /[A-Z]/.test(password);
     const hasLowerCase = /[a-z]/.test(password);
     const hasDigit = /\d/.test(password);
-    const hasSpecialChar = /[!@#$%^&*,.?":|<>]/.test(password);
+    const hasSpecialChar = /[!@#$%^&*,.?":|]/.test(password);
 	const hasForbiddenChar = /[<>(){}]/.test(password);
     const isLongEnough = password.length >= 8 && password.length <= 25;
 
