590537867578f69e1dde150d6821110054b5ee97
Anais Augu <aaugu@c2r8s4.42lausanne.ch>
2024-10-04 15:17:40 +0200

    asgi debug ok

--- aetheryte_api_gateway/app/aetheryte/aetheryte/asgi.py
+++ aetheryte_api_gateway/app/aetheryte/aetheryte/asgi.py
@@ -12,7 +12,7 @@ import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-from livechat.routing import websocket_urlpatterns
+from livechat.routing import chat_websocket_urlpatterns
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'aetheryte.settings')
 
